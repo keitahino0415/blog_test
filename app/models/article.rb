56b9674ba80ee40166457ea21cbe58f7955fd520
keitahino0415 <keitahino0415@gmail.com>
2018-01-04 00:50:26 +0900

    coment

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,10 @@
 class Article < ApplicationRecord
+  #重複許さんやつ
   validates :title, uniqueness:true
+
+  #15文字以上は許さんやつ
   validates :title, length: {maximum:15}
+
+  #30文字以上は許さんやつ
   validates :text, length: {maximum:30}
 end
